920fb951428ff323bc65086f53b17a22daeb8cf4
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-02-13 08:06:29 +0000

    Renders without errors.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 //  setup SVG with width and height based on viewport 
 let margin = 20,
-    width = parseInt(d3.select(".svgContainer").style("width")),
-    height = parseInt(d3.select(".svgContainer").style("height"));
+    width = parseInt(d3.select(".svg-container").style("width")),
+    height = parseInt(d3.select(".svg-container").style("height"));
+
+let months = ["January", "February", "March", "April", "May", "June", 
+              "July", "August", "September", "October", "November", "December" ];
 
 // x scale - time , years 
 let x = d3.scaleTime()
   .range([0, width]);
 
 // y scale - time, months Jan-Dec
-var y = d3.scaleOridinal()
-  .range([height, 0]);
+var y = d3.scaleOrdinal(months);
 
 // Axis
 var xAxis = d3.axisBottom(x)
@@ -21,14 +23,14 @@ var svg = d3.select(".heat-map")
     .attr("height", height + margin*2 )
     .attr("class", "heat-map")
   .append("g")
-    .attr("transform", "translate(" + margin + "," + margin + ")")
-   .call(tip);
+    .attr("transform", "translate(" + margin + "," + margin + ")");
+  // .call(tip);
 
-chart.append("g")
+svg.append("g")
    .attr("class", "x axis")
    .attr("transform", "translate(0," + height + ")")
    .call(xAxis);
 
-chart.append("g")
+svg.append("g")
   .attr("class", "y axis")
   .call(yAxis);
