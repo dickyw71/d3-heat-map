f579df25e8e054939b7fd9b467f45fcf3c041bc3
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-02-14 19:37:42 +0000

    Added color scale and trying to get data to render as rects.

--- js/index.js
+++ js/index.js
@@ -10,9 +10,11 @@ let months = ["January", "February", "March", "April", "May", "June",
 let x = d3.scaleTime()
   .range([0, width]);
 
-// y scale - time, months Jan-Dec
+// y scale - months Jan-Dec
 var y = d3.scaleOrdinal(months)
   .range([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 // Axis
 var xAxis = d3.axisBottom(x)
@@ -41,7 +43,21 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
 
   console.log(data);
 
-  y.domain([d3.map(data.monthlyVariance, function(d) { return d.month; }), 1]);
+  // x.domain([d3.filter()])
+  // y.domain([d3.map(data.monthlyVariance, function(d) { return d.month; }), 1]);
 
+  svg.selectAll(".slice")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "slice")
+      .attr("width", 5)
+      .attr("height", height/12)
+      .attr("x", function(d) { console.log(x(d.monthlyVariance.year)); return x(d.monthlyVariance.year); })
+      .attr("y", function(d) { console.log(y(d.monthlyVariance.month)); return y(d.appendmonthlyVariance.month); })
+      .style("fill", function(d) { color(d.baseTemperature + d.monthlyVariance.variance); return color(d.baseTemperature + d.monthlyVariance.variance); });
+    // .on("mouseover", function(d, i) {
+    //   tip.show(d, svg)
+    //   })
+    // .on("mouseout", tip.hide);
 });
  
