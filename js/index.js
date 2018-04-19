235edeb8f01ee807a3d6bd5fdae3d8e10d2bdf3d
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-04-19 22:45:34 +0100

    Added legend for temperature colours.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-let margin = { top: 20, left: 90, bottom: 40, right: 20 }
+let margin = { top: 20, left: 90, bottom: 70, right: 20 }
     width = 1100 - (margin.left + margin.right),
-    height = 600 - (margin.left + margin.right);
+    height = 600 - (margin.top + margin.bottom);
 
 let parseYear = d3.timeParse("%Y");
 let parseMonth = d3.timeParse("%B"); 
@@ -8,6 +8,7 @@ let parseMonth = d3.timeParse("%B");
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 let baselineTemp = 15;
+let myData = [];
 
 
 let stripeTip = d3.tip()
@@ -24,8 +25,8 @@ let x = d3.scaleTime()
 let y = d3.scaleTime()
   .range([height, 0]);
 
-// colour scale - cylindrical rainbow
-let colour = d3.scaleSequential(d3.interpolateRainbow);
+// colour scale 
+let colour = d3.scaleSequential(d3.interpolateInferno);
 
 // Axis
 let xAxis = d3.axisBottom(x)
@@ -69,12 +70,34 @@ svg.selectAll(".y-label")
   .text(d => d)
 
 
+svg.append("g")
+  .attr("class", "legendSequential")
+  .attr("transform", "translate(" + (width-360) + "," + (height+(margin.bottom/2)) + ")")
+  .append("text")
+    .attr("fill", "#000")
+    .attr("text-anchor", "start")
+    .attr("y", 30)
+    .text("Temperature ℃")
+
+let legendSequential = d3.legendColor()
+    .shapeWidth(25)
+    .cells([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+    .labelFormat(d3.format(".2"))
+    .orient("horizontal")
+    .labelOffset(-9)
+    .scale(colour) 
+    .on("cellover", function(d) { 
+      console.log(d)
+      displayOnly(d)
+    })
+              
+
 d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json", function(error, data) {
   if (error) throw error;
 
   baselineTemp = data.baseTemperature;
 
-  let myData = data.monthlyVariance;
+  myData = data.monthlyVariance;
   
   let first = data.monthlyVariance[0];
   let last = data.monthlyVariance[data.monthlyVariance.length-1];
@@ -84,10 +107,15 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
 
   x.domain([new Date(first.year, first.month, 1), new Date(last.year, last.month, 1)])
   y.domain([12, 1]);
-  colour.domain([maxTemp, minTemp])
-
+  colour.domain([minTemp.toPrecision(2), maxTemp.toPrecision(2)])
+  
   let stripeWidth = x.range()[1] / years;
   let stripeHeight = y.range()[0] / 12;
+  
+  console.log(stripeWidth, stripeHeight)
+
+  svg.select(".legendSequential")
+   .call(legendSequential)
 
   svg.append("g")
    .attr("class", "x axis")
@@ -115,5 +143,20 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
           stripeTip.show(d, svg)
        })
     .on("mouseout", stripeTip.hide);
+    
 });
  
+function displayOnly(temperature) {
+  // do stuff
+  let filteredData = d3.map(myData, (d) => ((baselineTemp + d.variance).toPrecision(1) <= temperature))
+  console.log(filteredData)
+
+  svg.selectAll(".stripe")
+    .exit().remove()
+
+  svg.selectAll(".stripe")  
+    .data(filteredData)
+  
+  svg.selectAll(".stripe")
+    .style("fill", (d) =>  colour(baselineTemp + d.variance))
+}
